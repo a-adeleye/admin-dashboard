d33a48c41642ee069e34f9e80cc678016c1efdc9
aadeleye <adeleyeadeyemib@gmail.com>
2022-03-01 15:48:09 +0100

    media query done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navigation from "./components/Navigation";
 import Main from "./components/Main";
 import Profile from "./components/Profile";
+import BottomNav from "./components/BottomNav";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navigation />
       <Main />
       <Profile />
+      <BottomNav />
     </div>
   );
 }
---
+++ src/components/BottomNav.js
@@ -0,0 +1,16 @@
+function BottomNav() {
+  return (
+    <section className="bottom-nav">
+      <i className="fa-solid fa-table-columns"></i>
+      <i className="fa-solid fa-calendar-days"></i>
+      <i className="fa-solid fa-gear"></i>
+      <i className="fa-solid fa-circle-plus"></i>
+      <i className="fa-regular fa-square-check"></i>
+      <i className="fa-regular fa-clock"></i>
+      <i className="fa-regular fa-bookmark"></i>
+      <i className="fa-solid fa-plus"></i>
+    </section>
+  );
+}
+
+export default BottomNav;
